9ec0843a9902a9bc4134300137260bfbc06c9d79
kev-techi <kevin@techi.be>
2025-01-13 23:36:25 +0100

    only enable minting if role

--- src/cw/index.ts
+++ src/cw/index.ts
@@ -1,5 +1,17 @@
-import { CommunityConfig, type Config } from "@citizenwallet/sdk";
+import {
+  CommunityConfig,
+  getAccountAddress,
+  hasRole,
+  MINTER_ROLE,
+  type Config,
+} from "@citizenwallet/sdk";
 import communities from "./communities.json";
+import { JsonRpcProvider, Wallet } from "ethers";
+
+export interface CommunityChoice {
+  name: string;
+  value: string;
+}
 
 export const getCommunity = (alias: string): CommunityConfig => {
   const community = communities.find(
@@ -15,9 +27,50 @@ export const getCommunities = (): CommunityConfig[] => {
   return communities.map((c: Config) => new CommunityConfig(c));
 };
 
-export const getCommunityChoices = (): { name: string; value: string }[] => {
+export const getCommunityChoices = (): CommunityChoice[] => {
   return communities.map((c: Config) => ({
     name: c.community.name,
     value: c.community.alias,
   }));
+};
+
+export const getCommunitiesWithMinterRole = async (): Promise<
+  CommunityChoice[]
+> => {
+  const choices: CommunityChoice[] = [];
+
+  for (const community of getCommunities()) {
+    const privateKey = process.env.BOT_PRIVATE_KEY;
+    if (!privateKey) {
+      continue;
+    }
+
+    const signer = new Wallet(privateKey);
+
+    const signerAccountAddress = await getAccountAddress(
+      community,
+      signer.address
+    );
+    if (!signerAccountAddress) {
+      continue;
+    }
+
+    const provider = new JsonRpcProvider(community.primaryRPCUrl);
+
+    if (
+      await hasRole(
+        community.primaryToken.address,
+        MINTER_ROLE,
+        signerAccountAddress,
+        provider
+      )
+    ) {
+      choices.push({
+        name: community.community.name,
+        value: community.community.alias,
+      });
+    }
+  }
+
+  return choices;
 };
--- src/register-commands.ts
+++ src/register-commands.ts
@@ -1,136 +1,142 @@
 import { REST, Routes } from "discord.js";
 import "dotenv/config";
-import { getCommunityChoices } from "./cw";
+import {
+  CommunityChoice,
+  getCommunitiesWithMinterRole,
+  getCommunityChoices,
+} from "./cw";
 
-const choices = getCommunityChoices();
-
-const commands = [
-  {
-    name: "ping",
-    description: "Replies with Pong!",
-  },
-  {
-    name: "balance",
-    description: "Replies with your balance!",
-    options: [
-      {
-        name: "token",
-        description: "The token to check",
-        type: 3, // STRING type
-        required: true,
-        choices,
-      },
-    ],
-  },
-  {
-    name: "address",
-    description: "Replies with your address!",
-    options: [
-      {
-        name: "token",
-        description: "The token to check",
-        type: 3, // STRING type
-        required: true,
-        choices,
-      },
-    ],
-  },
-  {
-    name: "send",
-    description: "Send a token to someone! 🚀",
-    options: [
-      {
-        name: "token",
-        description: "The token to send",
-        type: 3, // STRING type
-        required: true,
-        choices,
-      },
-      {
-        name: "user",
-        description: "The recipient's @username or 0x address",
-        type: 3, // STRING type
-        required: true,
-      },
-      {
-        name: "amount",
-        description: "The amount to send",
-        type: 10, // NUMBER type
-        required: true,
-      },
-      {
-        name: "message",
-        description: "The message to send",
-        type: 3, // STRING type
-        required: true,
-      },
-    ],
-  },
-  {
-    name: "mint",
-    description: "Mint a token for someone! 🔨",
-    default_member_permissions: "32",
-    options: [
-      {
-        name: "token",
-        description: "The token to mint",
-        type: 3, // STRING type
-        required: true,
-        choices,
-      },
-      {
-        name: "user",
-        description: "The recipient's @username or 0x address",
-        type: 3, // STRING type
-        required: true,
-      },
-      {
-        name: "amount",
-        description: "The amount to mint",
-        type: 10, // NUMBER type
-        required: true,
-      },
-      {
-        name: "message",
-        description: "The message to include",
-        type: 3, // STRING type
-        required: true,
-      },
-    ],
-  },
-  {
-    name: "burn",
-    description: "Burn a token from someone! 🔥",
-    default_member_permissions: "32",
-    options: [
-      {
-        name: "token",
-        description: "The token to burn",
-        type: 3, // STRING type
-        required: true,
-        choices,
-      },
-      {
-        name: "user",
-        description: "The recipient's @username or 0x address",
-        type: 3, // STRING type
-        required: true,
-      },
-      {
-        name: "amount",
-        description: "The amount to burn",
-        type: 10, // NUMBER type
-        required: true,
-      },
-      {
-        name: "message",
-        description: "The message to include",
-        type: 3, // STRING type
-        required: true,
-      },
-    ],
-  },
-] as const;
+const getCommands = (
+  choices: CommunityChoice[],
+  minterChoices: CommunityChoice[]
+) =>
+  [
+    {
+      name: "ping",
+      description: "Replies with Pong!",
+    },
+    {
+      name: "balance",
+      description: "Replies with your balance!",
+      options: [
+        {
+          name: "token",
+          description: "The token to check",
+          type: 3, // STRING type
+          required: true,
+          choices,
+        },
+      ],
+    },
+    {
+      name: "address",
+      description: "Replies with your address!",
+      options: [
+        {
+          name: "token",
+          description: "The token to check",
+          type: 3, // STRING type
+          required: true,
+          choices,
+        },
+      ],
+    },
+    {
+      name: "send",
+      description: "Send a token to someone! 🚀",
+      options: [
+        {
+          name: "token",
+          description: "The token to send",
+          type: 3, // STRING type
+          required: true,
+          choices,
+        },
+        {
+          name: "user",
+          description: "The recipient's @username or 0x address",
+          type: 3, // STRING type
+          required: true,
+        },
+        {
+          name: "amount",
+          description: "The amount to send",
+          type: 10, // NUMBER type
+          required: true,
+        },
+        {
+          name: "message",
+          description: "The message to send",
+          type: 3, // STRING type
+          required: true,
+        },
+      ],
+    },
+    {
+      name: "mint",
+      description: "Mint a token for someone! 🔨",
+      default_member_permissions: "32",
+      options: [
+        {
+          name: "token",
+          description: "The token to mint",
+          type: 3, // STRING type
+          required: true,
+          choices: minterChoices,
+        },
+        {
+          name: "user",
+          description: "The recipient's @username or 0x address",
+          type: 3, // STRING type
+          required: true,
+        },
+        {
+          name: "amount",
+          description: "The amount to mint",
+          type: 10, // NUMBER type
+          required: true,
+        },
+        {
+          name: "message",
+          description: "The message to include",
+          type: 3, // STRING type
+          required: true,
+        },
+      ],
+    },
+    {
+      name: "burn",
+      description: "Burn a token from someone! 🔥",
+      default_member_permissions: "32",
+      options: [
+        {
+          name: "token",
+          description: "The token to burn",
+          type: 3, // STRING type
+          required: true,
+          choices: minterChoices,
+        },
+        {
+          name: "user",
+          description: "The recipient's @username or 0x address",
+          type: 3, // STRING type
+          required: true,
+        },
+        {
+          name: "amount",
+          description: "The amount to burn",
+          type: 10, // NUMBER type
+          required: true,
+        },
+        {
+          name: "message",
+          description: "The message to include",
+          type: 3, // STRING type
+          required: true,
+        },
+      ],
+    },
+  ] as const;
 
 const token = process.env.DISCORD_TOKEN;
 const clientId = process.env.CLIENT_ID;
@@ -143,6 +149,13 @@ export const registerCommands = async () => {
 
   try {
     console.log("Started refreshing application (/) commands.");
+
+    const choices = getCommunityChoices();
+    const minterChoices = await getCommunitiesWithMinterRole();
+
+    console.log(minterChoices);
+
+    const commands = getCommands(choices, minterChoices);
 
     await rest.put(Routes.applicationCommands(clientId), { body: commands });
 
